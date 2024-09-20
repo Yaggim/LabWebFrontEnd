05c75d69ca00c08fb61f9a03b02b559fcdf6f076
UzalNicolas <nicolas.uzaltebes@gmail.com>
2024-09-20 00:40:58 -0300

    Subida de HTML, imagenes

---
+++ producto.js
@@ -0,0 +1,23 @@
+/*
+Botones de incremento y decremento
+
+    //
+    const decrementBtn = document.getElementById('decrement');
+    const incrementBtn = document.getElementById('increment');
+    const quantityInput = document.getElementById('quantity');
+
+    // Decrementar
+    decrementBtn.addEventListener('click', function() {
+        let currentValue = parseInt(quantityInput.value);
+        if (currentValue > 1) {
+            quantityInput.value = currentValue - 1;
+        }
+    });
+
+    // Incrementar
+    incrementBtn.addEventListener('click', function() {
+        let currentValue = parseInt(quantityInput.value);
+        quantityInput.value = currentValue + 1;
+    });
+
+*/
