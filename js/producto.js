f699823273beab36007f95418959111d846879cd
UzalNicolas <nicolas.uzaltebes@gmail.com>
2024-09-21 15:41:55 -0300

    Se agregaron funcionalidades al carrito y validaciones respecto al stock

--- js/producto.js
+++ js/producto.js
@@ -26,84 +26,95 @@ const productos = [
     }
 ];
 
-// Función para buscar un producto por su ID y cargar los datos al HTML
-function cargarProductoPorID(idProducto) {
-    // Buscar el producto por su ID
-    const producto = productos.find(prod => prod.id === idProducto);
-    let divStock = document.getElementById("divStock");
+// Llamada a la función principal con el ID del producto
+cargarProductoPorID(1);
 
+function cargarProductoPorID(idProducto) {
+    // Buscar el producto por el ID
+    const producto = productos.find(prod => prod.id === idProducto);
+
+    // Cargar los datos en la página 
     if (producto) {
         document.getElementById("producto-id").textContent = "Código de identificación de producto: " + producto.id;
         document.getElementById("producto-brand-model").textContent = producto.brand.name + " " + producto.model.name;
         document.getElementById("producto-category").textContent = "Categoría: " + producto.category.name;
-        document.getElementById("producto-priceARS").textContent = "Precio ARS: $" + producto.priceARS; //+ " / USD: $" + producto.priceUSD;
-        //document.getElementById("producto-stock").textContent = "Stock: " + producto.stock + " unidades";
+        document.getElementById("producto-priceARS").textContent = "Precio: $" + producto.priceARS; //+ " / USD: $" + producto.priceUSD;
         document.getElementById("producto-image1").src = producto.image[0];
         document.getElementById("producto-image2").src = producto.image[1];
         document.getElementById("producto-image3").src = producto.image[2];
         document.getElementById("producto-description").textContent = producto.description;
-        if(producto.stock < 10){
+        let divStock = document.getElementById("divStock");
+        if (producto.stock < 10) {
             document.querySelector("#divStock h2").innerHTML = "BAJO"
             divStock.style.backgroundColor = "#e72c2c"
         }
-        if(producto.stock >= 10){
+        if (producto.stock >= 10) {
             document.querySelector("#divStock h2").innerHTML = "ALTO"
             divStock.style.backgroundColor = "#7ac94c"
         }
+
+        // Incremento y decremento de cantidad seleccionada del producto
+
+        const decrement = document.getElementById('btnDecrement');
+        const increment = document.getElementById('btnIncrement');
+        const cantidad = document.getElementById('quantity');
+        decrement.addEventListener('click', function decrementar() {
+            let valorActual = parseInt(cantidad.value);
+            if (valorActual > 1) {
+                cantidad.value = valorActual - 1;
+            }
+        });
+        increment.addEventListener('click', function incrementar() {
+            let valorActual = parseInt(cantidad.value);
+            if (valorActual < producto.stock) {
+                cantidad.value = valorActual + 1;
+            }
+        });
+
+        // Comprar producto
+        // INCOMPLETO
+        document.getElementById("btnCompra").addEventListener("click", realizarCompra);
+        function realizarCompra(idProducto) {
+            if (producto.stock > 0) {
+                alert("(EL PRODUCTO CUENTA CON DISPONIBILIDAD) -> DIRIGE A PASARELA DE PAGO")
+            }
+
+        }
+
+        // Formulario de datos del modal envío a domicilio
+        document.getElementById("formEnvio").addEventListener("submit", function (event) {
+            event.preventDefault();
+            let calle = document.getElementById("envioCalle").value;
+            let altura = document.getElementById("envioAltura").value;
+            let postal = document.getElementById("envioPostal").value;
+            let nota = document.getElementById("envioNota").value;
+            console.log("Calle:", calle, "Altura:", altura, "Código Postal:", postal, "Nota:", nota);
+            alert("Envío cargado satisfactoriamente!");
+            let envioModal = bootstrap.Modal.getInstance(document.getElementById("modalEnvio"));
+            envioModal.hide();
+        });
+
+        // Mostrar modal de envío a domicilio
+        document.getElementById("btnEnvio").addEventListener("click", envioDomicilio);
+        function envioDomicilio() {
+            let envioModal = new bootstrap.Modal(document.getElementById("modalEnvio"));
+            envioModal.show();
+        }
+
+        // Agregar al carrito
+        document.getElementById("btnCarrito").addEventListener("click", agregarAlCarrito);
+        function agregarAlCarrito() {
+            let carritoModal = new bootstrap.Modal(document.getElementById("modalCarrito"));
+            let carrito = document.getElementById("modalCarrito")
+            carrito.querySelector(".modal-body").innerHTML = "Se ha añadido al carrito exitosamente: <br>" + producto.brand.name + " " + producto.model.name
+            carrito.querySelector("div.cantidadCarrito").textContent = "Cantidad: " + cantidad.value
+            carritoModal.show();
+        }
+
+
+        // Si no se encuentra el producto con el ID
     } else {
         console.error("Producto no encontrado.");
     }
 }
 
-// Llamada a la función con el ID del producto
-cargarProductoPorID(1);
-
-//Comprar
-document.getElementById("btnCompra").addEventListener("click", realizarCompra);
-function realizarCompra() {
-    //evaluar el stock de la bbdd >= const cantidad = document.getElementById('quantity');
-    alert("Se redirige a la pasarela de pago.");
-}
-
-document.getElementById("formEnvio").addEventListener("submit", function (event) {
-    event.preventDefault();
-
-    let calle = document.getElementById("envioCalle").value;
-    let altura = document.getElementById("envioAltura").value;
-    let postal = document.getElementById("envioPostal").value;
-    let nota = document.getElementById("envioNota").value;
-    console.log("Calle:", calle, "Altura:", altura, "Código Postal:", postal, "Nota:", nota);
-    alert("Envío cargado satisfactoriamente!");
-    let envioModal = bootstrap.Modal.getInstance(document.getElementById("modalEnvio"));
-    envioModal.hide();
-});
-
-//Enviar a domicilio
-document.getElementById("btnEnvio").addEventListener("click", envioDomicilio);
-function envioDomicilio() {
-    let envioModal = new bootstrap.Modal(document.getElementById("modalEnvio"));
-    envioModal.show();
-}
-
-//Agregar al carrito FALTA QUE AGREGUE AL MODAL LA LEYENDA DE BRAND + MODEL
-document.getElementById("btnCarrito").addEventListener("click", agregarAlCarrito);
-function agregarAlCarrito() {
-    let carritoModal = new bootstrap.Modal(document.getElementById("modalCarrito"));
-    carritoModal.show();
-}
-
-//Incremento y decremento de cantidad del producto
-const decrement = document.getElementById('btnDecrement');
-const increment = document.getElementById('btnIncrement');
-const cantidad = document.getElementById('quantity');
-decrement.addEventListener('click', function incrementar() {
-    let valorActual = parseInt(cantidad.value);
-    if (valorActual > 1) {
-        cantidad.value = valorActual - 1;
-    }
-});
-//Falta condicionar la cantidad sujeto al stock del producto producto.json "stock"
-increment.addEventListener('click', function decrementar() {
-    let valorActual = parseInt(cantidad.value);
-    cantidad.value = valorActual + 1;
-});
