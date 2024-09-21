94647390e6ae1203e8b80dfa3124df0d2b8e7fb5
UzalNicolas <nicolas.uzaltebes@gmail.com>
2024-09-21 01:42:45 -0300

    Se corrigieron los link de styles, se agregó modal de envío y código JS

--- js/producto.js
+++ js/producto.js
@@ -11,11 +11,11 @@
 //Insertar HTML de precio producto.json "priceARS"
 
 //Manejo de clase de divStock según el stock producto.json "stock"
-    /*
-    stock bajo #e72c2c
-    stock intermedio #eeee1e
-    stock alto #7ac94c
-    */
+/*
+stock bajo #e72c2c
+stock intermedio #eeee1e
+stock alto #7ac94c
+*/
 
 //Comprar
 document.getElementById("btnCompra").addEventListener("click", realizarCompra);
@@ -24,17 +24,31 @@ function realizarCompra() {
     alert("Se redirige a la pasarela de pago.");
 }
 
+document.getElementById("formEnvio").addEventListener("submit", function(event) {
+    event.preventDefault();
+    
+    let calle = document.getElementById("envioCalle").value;
+    let altura = document.getElementById("envioAltura").value;
+    let postal = document.getElementById("envioPostal").value;
+    let nota = document.getElementById("envioNota").value;
+    console.log("Calle:", calle, "Altura:", altura, "Código Postal:", postal, "Nota:", nota);
+    alert("Envío cargado satisfactoriamente!");
+    let envioModal = bootstrap.Modal.getInstance(document.getElementById("modalEnvio"));
+    envioModal.hide();
+});
+
 //Enviar a domicilio
 document.getElementById("btnEnvio").addEventListener("click", envioDomicilio);
 function envioDomicilio() {
-    alert("Has seleccionado envío a domicilio.");
+    let envioModal = new bootstrap.Modal(document.getElementById("modalEnvio"));
+    envioModal.show();
 }
 
 //Agregar al carrito
 document.getElementById("btnCarrito").addEventListener("click", agregarAlCarrito);
 function agregarAlCarrito() {
-    let miModal = new bootstrap.Modal(document.getElementById("modalCarrito"));
-    miModal.show();
+    let carritoModal = new bootstrap.Modal(document.getElementById("modalCarrito"));
+    carritoModal.show();
 }
 
 //Incremento y decremento de cantidad del producto
