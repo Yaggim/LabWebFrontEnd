f4c47cc74e5a30017d630b9c06feb2a18fe56512
UzalNicolas <nicolas.uzaltebes@gmail.com>
2024-09-29 20:06:41 -0300

    Se valida via JS los datos de la compra y se muestran los errores

--- js/producto.js
+++ js/producto.js
@@ -279,29 +279,38 @@ function cargarProductoPorID(idProducto) {
         // Formulario de datos del modal envío a domicilio
         // FALTARÍA VALIDAR TAMBIÉN POR ACÁ LOS DATOS DEL FORMULARIO
         document.getElementById("formCompra").addEventListener("submit", function (event) {
+            document.getElementById("envioTitular").classList.remove('is-invalid');
+            document.getElementById("envioCalle").classList.remove('is-invalid');
+            document.getElementById("envioAltura").classList.remove('is-invalid');
+            document.getElementById("envioPostal").classList.remove('is-invalid');
+            document.getElementById("envioNota").classList.remove('is-invalid');
+            document.getElementById("dni").classList.remove('is-invalid');
+            document.getElementById("tarjeta").classList.remove('is-invalid');
             event.preventDefault();
-            // Descuenta stock al producto
-            producto.stock -= cantidad.value;
-            // Solo para control de los campos ////////////////////////////////////////////////
-            console.log("Stock actual del producto: " + producto.stock)
-            let dni = document.getElementById("envioTitular").value;
-            let titular = document.getElementById("envioTitular").value;
-            let calle = document.getElementById("envioCalle").value;
-            let altura = document.getElementById("envioAltura").value;
-            let postal = document.getElementById("envioPostal").value;
-            let nota = document.getElementById("envioNota").value;
-            let pago = document.getElementById("tarjeta").value;
-            let articulo = [producto.brand, producto.model, producto.id];
-            console.log("DNI: ".dni, "Titular:", titular, "Calle:", calle, "Altura:", altura, "Código Postal:", postal, "Nota:", nota, "Pago efectuado:", pago, "Prod:", articulo, "Cantidad:", cantidad.value);
-            ////////////////////////////////////////////////////////////////////////////////////
-            // Setea de nuevo la cantidad a elegir para que comience en 1
-            cantidad.value = 1;
-            let compraModal = bootstrap.Modal.getInstance(document.getElementById("modalCompra"));
-            compraModal.hide();
-            let finCompraModal = new bootstrap.Modal(document.getElementById("modalFinCompra"));
-            finCompraModal.show();
-            // Vuelvo a evaluar el stock para completar el recuadro de "Stock en la web"
-            completarRecuadroStock(idProducto)
+            if (validarDatos()) {
+                // Descuenta stock al producto
+                producto.stock -= cantidad.value;
+                // Solo para control de los campos ////////////////////////////////////////////////
+                console.log("Stock actual del producto: " + producto.stock)
+                let dni = document.getElementById("envioTitular").value;
+                let titular = document.getElementById("envioTitular").value;
+                let calle = document.getElementById("envioCalle").value;
+                let altura = document.getElementById("envioAltura").value;
+                let postal = document.getElementById("envioPostal").value;
+                let nota = document.getElementById("envioNota").value;
+                let pago = document.getElementById("tarjeta").value;
+                let articulo = [producto.brand, producto.model, producto.id];
+                console.log("DNI: ".dni, "Titular:", titular, "Calle:", calle, "Altura:", altura, "Código Postal:", postal, "Nota:", nota, "Pago efectuado:", pago, "Prod:", articulo, "Cantidad:", cantidad.value);
+                ////////////////////////////////////////////////////////////////////////////////////
+                // Setea de nuevo la cantidad a elegir para que comience en 1
+                cantidad.value = 1;
+                let compraModal = bootstrap.Modal.getInstance(document.getElementById("modalCompra"));
+                compraModal.hide();
+                let finCompraModal = new bootstrap.Modal(document.getElementById("modalFinCompra"));
+                finCompraModal.show();
+                // Vuelvo a evaluar el stock para completar el recuadro de "Stock en la web"
+                completarRecuadroStock(idProducto)
+            }
         });
 
         // Agregar al carrito
@@ -408,6 +417,81 @@ function realizarCompra(idProducto) {
         divStock.style.backgroundColor = "#e72c2c"
         document.getElementById("btnCompra").disabled = true
         document.getElementById("btnCarrito").disabled = true
+    }
+}
+
+function validarDatos() {
+    let errores = 0;
+    //VALIDAR LARGO DEL NOMBRE
+    let nombre = document.getElementById("envioTitular").value
+    if(nombre.length < 3 || nombre.length > 50){
+        document.getElementById("envioTitular").classList.add('is-invalid');
+        errores += 1
+    }
+    //VALIDAR NOMBRE 2 PALABRAS
+    let palabras = nombre.split(/ +/)
+    if (palabras.length != 2) {
+        document.getElementById("envioTitular").classList.add('is-invalid');
+        errores += 1
+    }
+    //VALIDAR CARACTERES VALIDOS
+    let exRegular = /^[a-zA-ZáéíóúÁÉÍÓÚ ]+$/;
+    if (!exRegular.test(nombre)) {
+        document.getElementById("envioTitular").classList.add('is-invalid');
+        errores += 1
+    }
+    //VALIDAR CALLE
+    let calle = document.getElementById("envioCalle").value
+    let exRegularCalle = /^[A-Za-z0-9.áéíóúÁÉÍÓÚ ]+$/;
+    if (!exRegularCalle.test(calle)) {
+        document.getElementById("envioCalle").classList.add('is-invalid');
+        errores += 1
+    }
+    if (calle.length < 3 || calle.length > 30){
+        document.getElementById("envioCalle").classList.add('is-invalid');
+        errores += 1
+    }
+    //VALIDAR ALTURA
+    let altura = document.getElementById("envioAltura").value
+    if (isNaN(altura) || altura < 1 || altura > 99999) {
+        document.getElementById("envioAltura").classList.add('is-invalid');
+        errores += 1
+    }
+    //VALIDAR CODIGO POSTAL
+    let cp = document.getElementById("envioPostal").value
+    if (isNaN(cp) || cp.length != 4){
+        document.getElementById("envioPostal").classList.add('is-invalid');
+        errores += 1
+    }
+    //VALIDAR NOTAS
+    let nota = document.getElementById("envioNota").value
+    if (nota.length > 200){
+        document.getElementById("envioNota").classList.add('is-invalid');
+        errores += 1
+    }
+    //VALIDAR DNI NUMERICO 
+    let dni = document.getElementById("dni").value
+    if (isNaN(dni)) {
+        document.getElementById("dni").classList.add('is-invalid');
+        errores += 1
+    }
+    //VALIDAR DNI ENTRE 100.000 Y 99.999.999
+    if (dni < 100000 || dni > 99999999 || dni.length == 0) {
+        document.getElementById("dni").classList.add('is-invalid');
+        errores += 1
+    }
+    //VALIDAR TARJETA
+    let tarjeta = document.getElementById("tarjeta").value
+    if (tarjeta.length != 12 || isNaN(tarjeta)){
+        document.getElementById("tarjeta").classList.add('is-invalid');
+        errores += 1
+    }
+
+    console.log("Errores capturados: "+ errores)
+    if (errores > 0){
+        return false
+    } else {
+        return true
     }
 }
 
