71e43d110608d63993591a1bd973eefdff19d1a9
Diego Marini <dlmarini2020@gmail.com>
2024-10-27 18:32:47 -0300

    Se generó clases para conexión y Crud genéricos por tabla

--- includes/conexion.php
+++ includes/conexion.php
@@ -1,16 +1,52 @@
 <?php
+    /**
+     * Esta clase ofrece métodos estáticos
+     * para conextarse a una BBDD.
+     * 
+     * 
+     */
+    class Conexion
+    {
+        private $servername  = "localhost";
+        private $username    = "root"     ;
+        private $password    = ""         ;
+        private $db          = 'hardtech';
+        private $pdo                      ;
+        private static $conn = null       ;
+        
 
-if (!isset($conn)) {
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $db = "hardtech2";
 
-    try {
-        $conn = new PDO("mysql:host=".$servername.";dbname=".$db.";charset=utf8", $username, $password);
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    } catch (PDOException $e) {
-        echo "Error de conexión: " . $e->getMessage();
-        $conn = null;
+        //constuctor
+        public function __construct(){
+            
+            try{
+                $this->pdo = new PDO("mysql:host=".$this->servername.";dbname=".$this->db.";charset=utf8", $this->username, $this->password);
+
+                $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                //echo "Conexion a ".$this->db." OK!!!!";
+
+            }catch(PDOException $e){
+                die("Error de conexión: ". $e->getMessage());
+            }
+        }
+
+
+        //estático para obtener conexión
+        public static function getConn(){
+            if(self::$conn == null){
+                self::$conn = new Conexion();
+            }
+
+            return self::$conn->pdo;
+        }
+
+
+        //estático para cerrar conexión
+        public static function closeConn(){
+            if(self::$conn != null){
+               self::$conn->pdo = null;
+               self::$conn      = null; 
+            }
+        }
     }
-}
+?>
