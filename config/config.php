59c4a0c0f5e8de702b72b3d05b6c49caecfa9770
Manuel Nicolás <mediavilla.grosjean@gmail.com>
2024-11-14 01:28:54 -0300

    Modificar archivo de configuración

--- config/config.php
+++ config/config.php
@@ -1,5 +1,7 @@
 <?php
 
+session_start();
+
 $ruta_base = str_replace('\\', '/', $_SERVER['DOCUMENT_ROOT']);
 $ruta_actual = str_replace('\\', '/', __DIR__);
 $ruta_relativa = str_replace($ruta_base, '', $ruta_actual);
@@ -8,3 +10,24 @@ $carpeta_proyecto = explode('/', trim($ruta_relativa, '/'))[0];
 define('CARPETA_PROYECTO', $carpeta_proyecto);
 
 define('RUTA_PROYECTO', $_SERVER['DOCUMENT_ROOT'].'/'.CARPETA_PROYECTO);
+
+function loadEnv($path = __DIR__.'/../.env') {
+    if (!file_exists($path)) {
+        die("Archivo .env no encontrado.");
+    }
+    $env = parse_ini_file($path);
+    foreach ($env as $key => $value) {
+        putenv("$key=$value");
+    }
+}
+
+loadEnv();
+
+// require_once RUTA_PROYECTO.'/includes/conexion_db.php';
+require_once RUTA_PROYECTO.'/includes/conexion.php';
+
+require_once RUTA_PROYECTO.'/includes/usuario.php';
+$usuario = new Usuario("usuarios");
+
+require_once RUTA_PROYECTO.'/includes/permisos.php';
+Permisos::setConexion();
