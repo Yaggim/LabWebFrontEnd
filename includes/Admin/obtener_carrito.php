f4645ec611a09663d2c8a6d5cd61ffddc759fd45
Leandro Villalba <villalbaleandrogabriel@gmail.com>
2024-11-13 21:36:34 -0300

    03 - Correcciones para obtener carrito

--- includes/Admin/obtener_carrito.php
+++ includes/Admin/obtener_carrito.php
@@ -2,9 +2,17 @@
 session_start();
 require_once '../includes/Admin/carrito.php';
 
-$userId = isset($_SESSION['user_id']) ? $_SESSION['user_id'] : null;
-$carrito = new Carrito($userId);
-$productosCarrito = $carrito->verCarrito();
+header('Content-Type: application/json');
 
-echo json_encode($productosCarrito);
+try {
+    $userId = isset($_SESSION['user_id']) ? $_SESSION['user_id'] : null;
+    $carrito = new Carrito($userId);
+    $productosCarrito = $carrito->verCarrito();
+
+    echo json_encode($productosCarrito);
+} catch (Exception $e) {
+    // Manejo de errores
+    http_response_code(500);
+    echo json_encode(['error' => $e->getMessage()]);
+}
 ?>
