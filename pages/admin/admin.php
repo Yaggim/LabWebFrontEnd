b7dd961cbaba4f09a79e9b10b62057debb6efb43
Yaggi <yaggim@gmail.com>
2024-11-17 18:26:07 -0300

    Permisos

--- pages/admin/admin.php
+++ pages/admin/admin.php
@@ -1,16 +1,16 @@
 <?php
     require_once(__DIR__ . "/../../config/config.php"); 
-    //session_start();
+
     if(!isset($_SESSION['usuario'])){
         header('Location: home');
     }
 
-    if (!Permisos::tienePermiso("Crear producto", $_SESSION['usuario']['id']) 
-        ||!Permisos::tienePermiso("Crear producto", $_SESSION['usuario']['id'])
-        ||!Permisos::tienePermiso("Modificar stock", $_SESSION['usuario']['id'])
-        ||!Permisos::tienePermiso("Crear combo", $_SESSION['usuario']['id'])
-        ||!Permisos::tienePermiso("Crear oferta semanal", $_SESSION['usuario']['id'])
-        ||!Permisos::tienePermiso("Gestionar ventas", $_SESSION['usuario']['id'])) {
+    if (!Permisos::tienePermiso("Crear producto", $_SESSION['usuario']['id']) && 
+        !Permisos::tienePermiso("Crear producto", $_SESSION['usuario']['id']) &&
+        !Permisos::tienePermiso("Modificar stock", $_SESSION['usuario']['id']) &&
+        !Permisos::tienePermiso("Crear combo", $_SESSION['usuario']['id']) && 
+        !Permisos::tienePermiso("Crear oferta semanal", $_SESSION['usuario']['id']) && 
+        !Permisos::tienePermiso("Gestionar ventas", $_SESSION['usuario']['id'])) {
         header("Location: home");
         die;
     }
--- pages/admin/categoria.php
+++ pages/admin/categoria.php
@@ -1,6 +1,6 @@
 <?php
     require_once(__DIR__ . "/../../config/config.php"); 
-    //session_start();
+
     if(!isset($_SESSION['usuario'])){
         header('Location: home');
     }
