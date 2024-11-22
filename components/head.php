7f9765c71798f3e71fbd7d3577ca1b49610bff74
MediavillaGrosjean <148763212+MediavillaGrosjean@users.noreply.github.com>
2024-11-22 19:13:01 -0300

    Refactor/navbar categorias buscador responsive (#41)
    * Agregar url de error y acomodar reglas en .htaccess
    
    * Corregir estilos y estructura HTML de página de error
    
    * Borrar links de categorías del navbar (desktop y mobile)
    
    * Agregar búsqueda de productos mobile y fix menores
    
    * Agregar carga dinámica de categorías en navbar (desktop y mobile)
    
    * Modificar lógica de productos para filtrar desde el navbar y agregar detalles menores de estilo
    
    * Agregar corrección menor en visualización de productos

--- components/head.php
+++ components/head.php
@@ -15,5 +15,6 @@
 <script type="text/javascript" src="/<?php echo CARPETA_PROYECTO ?>/js/login.js" defer></script>
 <script type="text/javascript" src="/<?php echo CARPETA_PROYECTO ?>/js/modal.js" defer></script>
 <script type="text/javascript" src="/<?php echo CARPETA_PROYECTO ?>/js/busquedaProducto.js" defer></script>
+<script type="text/javascript" src="/<?php echo CARPETA_PROYECTO ?>/js/navbarCategorias.js" defer></script>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous" defer></script>
